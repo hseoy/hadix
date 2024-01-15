a22dcd9971c5e746379c5b488a143343ffbea806
Yunseo Hwang <yunseo.dev@gmail.com>
2024-01-15 21:56:16 +0900

    feat: Light/Dark 모드 적용을 위한 기본 설정 추가 & 간단한 dark mode 처리

--- apps/web-client/src/modules/app-layout/components/AppBody/AppBody.tsx
+++ apps/web-client/src/modules/app-layout/components/AppBody/AppBody.tsx
@@ -1,9 +1,15 @@
 import { Stack } from '~/modules/common';
 
+import styles from './AppBody.module.scss';
+
 export interface AppBodyProps {
   children: React.ReactNode;
 }
 
 export const AppBody = ({ children }: AppBodyProps) => {
-  return <Stack fullHeight>{children}</Stack>;
+  return (
+    <Stack fullHeight className={styles['app-body']}>
+      {children}
+    </Stack>
+  );
 };
--- apps/web-client/vite.config.ts
+++ apps/web-client/vite.config.ts
@@ -1,8 +1,21 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '~': path.join(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "~/styles/variables";',
+      },
+    },
+  },
 });
