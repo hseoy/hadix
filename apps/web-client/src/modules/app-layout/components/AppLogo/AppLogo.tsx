98d128ca351e29573d816319307de712dfc226d6
Yunseo Hwang <yunseo.dev@gmail.com>
2024-01-20 14:26:14 +0900

    refactor: AppLogo에 root link 추가 & hover 시에만 애니메이션 적용

--- apps/web-client/src/modules/app-layout/components/AppLogo/AppLogo.tsx
+++ apps/web-client/src/modules/app-layout/components/AppLogo/AppLogo.tsx
@@ -2,11 +2,11 @@ import styles from './AppLogo.module.scss';
 
 export const AppLogo = () => {
   return (
-    <div className={styles.logo}>
+    <a href="/" className={styles.logo}>
       <span className={styles['animated-letter-container']}>
         <span className={styles['animated-letter']}>P</span>
       </span>
       <span>놀이터</span>
-    </div>
+    </a>
   );
 };
