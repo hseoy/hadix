336504f77d09673cd839917b43e9ad556347fb1f
Yunseo Hwang <yunseo.dev@gmail.com>
2024-09-22 23:44:44 +0900

    chore: 아직 개발중이므로 검색엔진 검색 안되도록 처리

--- apps/ui-builder/src/app/layout.tsx
+++ apps/ui-builder/src/app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Hadix - UI Builder',
   description: 'UI Builder for Hadix',
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
